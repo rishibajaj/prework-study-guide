9680f36b4e6c18adf6a40fcb8d2a0a124660931e
RishiBajaj <rishi.bajaj@icloud.com>
2022-11-30 23:21:48 +0000

    added script.js and changes for index.html

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+
+
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+
+var shapes = ["triangle", "square", "pentagon", "circle"];
+for (var x = 0; x < shapes.length; x++) {
+    console.log(shapes[x]);
+}
+
+var studentInfo = ["Lu", 54, true];
+for (var x= 0; x <studentInfo.length; x++){
+    console.log(studentInfo[x]);
+}
+
